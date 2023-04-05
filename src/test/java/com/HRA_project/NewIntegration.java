5f56a3933d9c2d61a924df61b84b4543d84ccd80
Admin <Admin@DESKTOP-P1FTGIL>
2023-04-05 15:38:05 +0530

    xcvdcvbs

---
+++ src/test/java/com/HRA_project/NewIntegration.java
@@ -0,0 +1,10 @@
+package com.HRA_project;
+
+public class NewIntegration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Integration Abhishek");
+	}
+
+}
