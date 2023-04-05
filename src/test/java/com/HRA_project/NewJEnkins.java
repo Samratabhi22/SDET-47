3acb2bb251c01dc8f1f4d282377e7f41b60859bb
Admin <Admin@DESKTOP-P1FTGIL>
2023-04-05 17:28:15 +0530

    learning jenkins

---
+++ src/test/java/com/HRA_project/NewJEnkins.java
@@ -0,0 +1,10 @@
+package com.HRA_project;
+
+public class NewJEnkins {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("learning jenkins sucessfully");
+	}
+
+
+}
